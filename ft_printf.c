4f10a9bfb1004200c747e5b3d6de56fbcc11ef63
upperdim <45048352+upperdim@users.noreply.github.com>
2023-10-20 19:55:54 +0200

    dev

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,13 @@
 /*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 16:20:53 by tunsal            #+#    #+#             */
-/*   Updated: 2023/10/20 17:37:15 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/10/20 19:55:35 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdarg.h>
+#include "ft_printf.h"
+
 /*
    TODO:
     - Don't implement the buff management of the original printf.
@@ -52,37 +53,47 @@ int	ft_printf(const char *str, ...)
 					break;
 				case 's':
 					char *param_str = va_arg(valist, char *);
-					ft_putstr(param_str);
-					print_count += ft_strlen(param_str);
+					print_count += ft_putstr(param_str);
 					break;
 				case 'p':
-					// TODO
+					// Take the (void *) argument whose value holds an address
+					// Print its value (address) in hex format
 					break;
 				case 'd':
 				case 'i':
 					int param_int = va_arg(valist, char);
-					ft_putnbr(param_int);
-					print_count += count_digits(param_int);
+					print_count += ft_putnbr(param_int);
 					break;
 				case 'u':
+					// Get the number whether dec, oct or hex
+					// Without modifying it, interpret its bits as base 10 unsigned decimal
+					// Print it
+					unsigned int param_uint = va_arg(valist, int);
+					print_count += ft_putnbr(param_uint);
 					break;
 				case 'x':
+					int param_int = va_arg(valist, int);
+					print_count += ft_putnbr_hex(param_int);
 					break;
 				case 'X':
+					int param_int = va_arg(valist, int);
+					print_count += ft_putnbr_hex_upcase(param_int);
 					break;
 				case '%':
+					ft_putchar('%');
+					++print_count;
 					break;
 				default:
-					// error
-					break;
+					return (-1);
 			}
 		}
 		else
 		{
-			// write the character
+			ft_putchar(*str);
 			++print_count;
 		}
 		++str;		
 	}
+	va_end(valist);
 	return (print_count);
 }
