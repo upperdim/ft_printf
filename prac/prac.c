75738f7abfd44ac3a8706af0c83215eaa39ab9f5
upperdim <45048352+upperdim@users.noreply.github.com>
2023-10-20 19:56:19 +0200

    added test area

--- prac/prac.c
+++ prac/prac.c
@@ -6,18 +6,37 @@ double get_average(size_t nums_count, ...) {
 	va_start(valist, nums_count);
 
 	double sum = 0.0;
-
+	for (size_t i = 0; i < nums_count; ++i) {
+		sum += va_arg(valist, double);
+	}
+	va_end(valist);
+	return sum / nums_count;
 }
 
 void d_and_i() {
-	printf("%d\n", 033);
-	printf("%i\n", 033);
+	printf("--------------------\n");
+	printf("d and i tests\n");
+	printf("010 with d = %d\n", 010);
+	printf("010 with i = %i\n", 010);
+	printf("--------------------\n");
+}
+
+void u() {
+	printf("--------------------\n");
+	printf("u tests\n");
+	printf("-1 with u = %u\n", -1);
+	printf("-10 with u = %u\n", -10);
+	printf("-010 with u = %u\n", -010);
+	printf("-0x10 with u = %u\n", -0x10);
+	printf("--------------------\n");
 }
 
 int main() {
-	d_and_i(); return 0;
+	// d_and_i();
+	// u();
+
 
 	double a = 13.37, b = 12.34, c = 98.76;
 
-	printf("Average = %lf\n", get_average(a, b, c));
+	printf("Average = %lf\n", get_average(3, a, b, c));
 }
