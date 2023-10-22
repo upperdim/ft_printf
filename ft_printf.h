c7cd9a7eee82c5c183318bd5c8ce5db75b457243
upperdim <45048352+upperdim@users.noreply.github.com>
2023-10-22 16:16:06 +0200

    added declarations

--- ft_printf.h
+++ ft_printf.h
@@ -2,5 +2,15 @@
 # define FT_PRINT
 
 #include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_putchar(char c);
+size_t	ft_putstr(char *s);
+size_t	ft_putnbr(int nb);
+
+size_t	ft_putnbr_base(int nbr, char *base);
+size_t	ft_putnbr_hex(int n);
+size_t	ft_putnbr_hex_upcase(int n);
 
 #endif
