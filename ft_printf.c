ff9ddb18202f7caf8e880390fbb032367b5aa471
upperdim <45048352+upperdim@users.noreply.github.com>
2023-10-23 18:14:59 +0200

    renaming and returning -1 if format is NULL

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 16:20:53 by tunsal            #+#    #+#             */
-/*   Updated: 2023/10/23 17:45:59 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/10/23 18:14:27 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,28 @@
 #define FAILURE -1
 
 /*
-   Handle symbols and update number of characters printed to the screen via
-   `p_print_cnt` parameter.
+   Handle symbols and update number of characters printed to the screen 
+   via `p_printed_cnt` parameter.
    Return 0 (defined as SUCCESS) if no errors, else -1 (defined as FAILURE).
 */
-static int	handle_symbls(const char *fmt, va_list *valist, int *p_print_cnt)
+static int	handle_symbols(const char *fmt, va_list *valist, int *p_printed_cnt)
 {
 	if (*fmt == 'c')
-		*p_print_cnt += ft_putchar(va_arg(*valist, int));
+		*p_printed_cnt += ft_putchar(va_arg(*valist, int));
 	else if (*fmt == 's')
-		*p_print_cnt += ft_putstr(va_arg(*valist, char *));
+		*p_printed_cnt += ft_putstr(va_arg(*valist, char *));
 	else if (*fmt == 'p')
-		*p_print_cnt += ft_putunbr_hex(va_arg(*valist, unsigned long));
+		*p_printed_cnt += ft_putunbr_ptr(va_arg(*valist, size_t), 1);
 	else if (*fmt == 'd' || *fmt == 'i')
-		*p_print_cnt += ft_putnbr(va_arg(*valist, int));
+		*p_printed_cnt += ft_putnbr(va_arg(*valist, int));
 	else if (*fmt == 'u')
-		*p_print_cnt += ft_putunbr(va_arg(*valist, unsigned int));
+		*p_printed_cnt += ft_putunbr(va_arg(*valist, unsigned int));
 	else if (*fmt == 'x')
-		*p_print_cnt += ft_putunbr_hex(va_arg(*valist, unsigned int));
+		*p_printed_cnt += ft_putunbr_hex(va_arg(*valist, unsigned int), 0);
 	else if (*fmt == 'X')
-		*p_print_cnt += ft_putunbr_hex_upcase(va_arg(*valist, unsigned int));
+		*p_printed_cnt += ft_putunbr_hex(va_arg(*valist, unsigned int), 1);
 	else if (*fmt == '%')
-		*p_print_cnt += ft_putchar('%');
+		*p_printed_cnt += ft_putchar('%');
 	else
 		return (FAILURE);
 	return (SUCCESS);
@@ -68,14 +68,14 @@ int	ft_printf(const char *fmt, ...)
 	int		print_count;
 
 	if (fmt == NULL)
-		return (0);
+		return (-1);
 	va_start(valist, fmt);
 	print_count = 0;
 	while (*fmt != '\0')
 	{
 		if (*fmt == '%')
 		{
-			if (handle_symbls(++fmt, &valist, &print_count) == FAILURE)
+			if (handle_symbols(++fmt, &valist, &print_count) == FAILURE)
 				return (-1);
 		}
 		else
