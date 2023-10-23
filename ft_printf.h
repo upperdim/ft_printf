964f63e559ecd81b18647bc2bb18c840a33b4770
upperdim <45048352+upperdim@users.noreply.github.com>
2023-10-23 15:28:45 +0200

    update header

--- ft_printf.h
+++ ft_printf.h
@@ -1,14 +1,18 @@
 #ifndef FT_PRINTF
-# define FT_PRINT
+# define FT_PRINTF
 
 #include <stdarg.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *fmt, ...);
 
 void	ft_putchar(char c);
 size_t	ft_putstr(char *s);
-size_t	ft_putnbr(int nb);
 
+size_t	ft_putnbr(int nb);
+size_t	ft_putunbr(unsigned int nb);
 size_t	ft_putnbr_base(int nbr, char *base);
 size_t	ft_putnbr_hex(int n);
 size_t	ft_putnbr_hex_upcase(int n);
