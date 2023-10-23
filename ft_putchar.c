66e0cdf88fa2b53b2037470add8b85f379b060e5
upperdim <45048352+upperdim@users.noreply.github.com>
2023-10-23 15:29:18 +0200

    include

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,9 +6,11 @@
 /*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 14:04:59 by tunsal            #+#    #+#             */
-/*   Updated: 2023/10/22 14:12:14 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/10/23 14:08:20 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 /* Write character `c` into standard output. */
 void	ft_putchar(char c)
