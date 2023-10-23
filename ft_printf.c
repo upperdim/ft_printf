ebf2ef348bf3070826e431323e23d5805c241cb1
upperdim <45048352+upperdim@users.noreply.github.com>
2023-10-23 17:38:33 +0200

    simplify main function

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,43 @@
 /*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 16:20:53 by tunsal            #+#    #+#             */
-/*   Updated: 2023/10/23 14:47:01 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/10/23 17:29:09 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "ft_printf.h"
+
+#define SUCCESS 0
+#define FAILURE -1
+
+/*
+   Handle symbols and update number of characters printed to the screen via
+   `p_print_cnt` parameter.
+   Return 0 (defined as SUCCESS) if no errors, else -1 (defined as FAILURE).
+*/
+static int	handle_symbls(const char *fmt, va_list *valist, int *p_print_cnt)
+{
+	if (*fmt == 'c')
+		*p_print_cnt += ft_putchar(va_arg(*valist, int));
+	else if (*fmt == 's')
+		*p_print_cnt += ft_putstr(va_arg(*valist, char *));
+	else if (*fmt == 'p')
+		*p_print_cnt += ft_putunbr_hex(va_arg(*valist, unsigned long));
+	else if (*fmt == 'd' || *fmt == 'i')
+		*p_print_cnt += ft_putnbr(va_arg(*valist, int));
+	else if (*fmt == 'u')
+		*p_print_cnt += ft_putunbr(va_arg(*valist, unsigned int));
+	else if (*fmt == 'x')
+		*p_print_cnt += ft_putunbr_hex(va_arg(*valist, unsigned int));
+	else if (*fmt == 'X')
+		*p_print_cnt += ft_putunbr_hex_upcase(va_arg(*valist, unsigned int));
+	else if (*fmt == '%')
+		*p_print_cnt += ft_putchar('%');
+	else
+		return (FAILURE);
+	return (SUCCESS);
+}
 
 /*
    TODO:
@@ -34,71 +65,21 @@
 int	ft_printf(const char *fmt, ...)
 {
 	va_list	valist;
-	size_t	print_count;
-	size_t	curr_placeholder_idx;
+	int		print_count;
 
 	if (fmt == NULL)
 		return (0);
-
 	va_start(valist, fmt);
-	curr_placeholder_idx = 0;
 	print_count = 0;
 	while (*fmt != '\0')
 	{
 		if (*fmt == '%')
 		{
-			int param_int;
-			char *param_str;
-			unsigned int param_uint;
-			
-			++fmt;
-			switch (*fmt)
-			{
-				case 'c':
-					ft_putchar(va_arg(valist, int));
-					++print_count;
-					break;
-				case 's':
-					param_str = va_arg(valist, char *);
-					print_count += ft_putstr(param_str);
-					break;
-				case 'p':
-					// Take the (void *) argument whose value holds an address
-					// Print its value (address) in hex format
-					break;
-				case 'd':
-				case 'i':
-					param_int = va_arg(valist, int);
-					print_count += ft_putnbr(param_int);
-					break;
-				case 'u':
-					// Get the number whether dec, oct or hex
-					// Without modifying it, interpret its bits as base 10 unsigned decimal
-					// Print it
-					param_uint = va_arg(valist, unsigned int);
-					print_count += ft_putunbr(param_uint);
-					break;
-				case 'x':
-					param_int = va_arg(valist, unsigned int);
-					print_count += ft_putnbr_hex(param_int);
-					break;
-				case 'X':
-					param_int = va_arg(valist, unsigned int);
-					print_count += ft_putnbr_hex_upcase(param_int);
-					break;
-				case '%':
-					ft_putchar('%');
-					++print_count;
-					break;
-				default:
-					return (-1);
-			}
+			if (handle_symbls(++fmt, &valist, &print_count) == FAILURE)
+				return (-1);
 		}
 		else
-		{
-			ft_putchar(*fmt);
-			++print_count;
-		}
+			print_count += ft_putchar(*fmt);
 		++fmt;		
 	}
 	va_end(valist);
