7df46f0b09d1f596cc7b9f58dea395802aaa161a
upperdim <45048352+upperdim@users.noreply.github.com>
2023-11-11 20:23:09 +0100

    spacing

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 17:39:41 by tunsal            #+#    #+#             */
-/*   Updated: 2023/10/24 16:57:59 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/11/11 20:19:28 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	ft_putstr(char *s);
 
 int	ft_putnbr(int nb);
 int	ft_putunbr(unsigned int nb);
+
 int	ft_putsize_base(size_t nbr, char *base);
 int	ft_putunbr_hex(unsigned int n, int use_uppercase_notation);
 int	ft_putunbr_ptr(size_t addr, int use_hex_prefix);
